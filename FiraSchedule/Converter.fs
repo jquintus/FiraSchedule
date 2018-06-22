b719dd470ea770f898bb983a4fa858415e0e4af3
Josh Quintus <josh@dotalign.com>
2018-06-22 14:36:17 -0400

    initial commit

---
+++ FiraSchedule/Converter.fs
@@ -0,0 +1,58 @@
+﻿module Converter
+
+open System
+
+let tableHeader = """
+    <table cellspacing="0" cellpadding="0">
+        <thead>
+            <th class="placeholder"></th>
+"""
+let tableFooter = "        </thead>"
+
+let getMonday (dt:DateTime) =
+    let delta = 
+        DayOfWeek.Monday - dt.DayOfWeek 
+        |> float
+    dt.AddDays(delta)
+
+let weeks (startDate:DateTime) (endDate:DateTime) =
+    let days = endDate - startDate
+    System.Math.Round (float days.Days / 7.0) |> int
+
+let month (date:DateTime) = 
+    match date.Month with
+    |  1 -> "Jan"
+    |  2 -> "Feb"
+    |  3 -> "Mar"
+    |  4 -> "Apr"
+    |  5 -> "May"
+    |  6 -> "Jun"
+    |  7 -> "Jul"
+    |  8 -> "Aug"
+    |  9 -> "Sep"
+    | 10 -> "Oct"
+    | 11 -> "Nov"
+    | 12 -> "Dec"
+    |  _ -> "???"
+
+let th (date:DateTime) = 
+    let mth = month date
+    sprintf "<th> %s-%i </th>" mth date.Day
+
+let addWeeks (date:DateTime) weekCount =
+    let daysToAdd = (weekCount * 7) |> float
+    date.AddDays(daysToAdd)
+
+let tableBody startDate endDate = 
+    let mutable body = ""
+    let firstMonday = getMonday startDate
+    let weekCount = weeks startDate endDate
+    for weekId in 1 .. weekCount do
+        let currentWeek = addWeeks firstMonday weekId
+        let currentTh = th currentWeek
+        body <- sprintf "%s            %s\n" body currentTh
+    body
+
+let generateThead startDate endDate =
+    let body = tableBody startDate endDate
+    sprintf "%s %s %s" tableHeader body tableFooter
---
+++ FiraSchedule/Program.fs
@@ -0,0 +1,49 @@
+﻿// Learn more about F# at http://fsharp.org
+
+open System
+open System.IO
+open System.Text
+open Converter
+
+let header = """
+<html>
+    <head>
+        <link rel="stylesheet" href="styles.css"/>
+    </head>
+<body>
+"""
+
+let footer = """
+    </table>
+<body>
+<html>
+"""
+
+let output = "C:\\test\\schedule.html"
+
+let append text (sb:StringBuilder) =
+    sb.AppendLine(text) 
+
+let sbToString (sb:StringBuilder) =
+    sb.ToString()
+
+let sbCreate =
+    new StringBuilder()
+
+let startDate = new DateTime (2018, 4, 25)
+let endDate = new DateTime (2018, 9, 3)
+
+[<EntryPoint>]
+let main argv =
+
+    let text = 
+        sbCreate        
+        |> append header
+        |> append (generateThead startDate endDate)
+        |> append footer        
+        |> sbToString
+
+    File.WriteAllText(output, text)
+
+    0
+
